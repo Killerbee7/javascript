d05f68d66e34c0aa120058129867d2a8b08ce40f
Dahal Dibya <s2201206@BC-C02J64AQQ6LR-MacBook-Air.local>
2022-09-05 13:23:21 +0300

    some updates

---
+++ ArrowFunction/arrowfunc05.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+In this challenge, you're asked to write the triple function from scratch as an arrow function.
+The function triple receives a parameter and returns the result of multiplying it by 3.
+**/
+
+
+const sum=(a,b) =>{
+
+    return (a+b)*3;
+
+}
+
+// (optional) Sample usage
+console.log(sum(2, 4));
