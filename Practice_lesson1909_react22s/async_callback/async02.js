cc8070d769161706b6ead57324bf6bd673cd971c
Dahal Dibya <s2201206@MacBook-Air.local>
2022-09-26 13:34:35 +0300

    updates on exercises

---
+++ Practice_lesson1909_react22s/async_callback/async02.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+Update the delayedWelcome function such that it delays the console.log
+call 1 second into the future.*/
+
+/**
+ * @param {string} name
+ */
+ const delayedWelcome = name => {
+        setTimeout(() => {
+                console.log("Hello World! Delayed");
+            }, 2000);
+            
+        }
+// Sample usage - do not modify
+console.log(delayedWelcome("Sam"));
+console.log(delayedWelcome("Alex"));
